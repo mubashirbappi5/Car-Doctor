f2bff3a05a1f6f9dc560d1bf09fb837f901b8b6b
mubashirbappi <mubashirbappi@gmail.com>
2025-02-04 20:07:14 +0600

    about section added

---
+++ src/Components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+
+            <div>
+
+            </div>
+            <div>
+                <h3>About Us</h3>
+                <h1>We are qualified & of experience in this field</h1>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <button className='btn'>Get More Info</button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default About;
--- src/Components/Navber.jsx
+++ src/Components/Navber.jsx
@@ -12,7 +12,7 @@ const Navber = () => {
     </>
     return (
         <div>
-            <div className="navbar bg-base-100 w-full  max-w-5xl mx-auto">
+            <div className="navbar bg-base-100 w-full max-w-5xl mx-auto">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
