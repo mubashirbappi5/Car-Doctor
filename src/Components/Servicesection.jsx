c0fc8eda0f26929775739a5aaf6b55682fa386d4
mubashirbappi <mubashirbappi@gmail.com>
2025-02-06 23:05:20 +0600

    mongodb connect

--- src/Components/Servicesection.jsx
+++ src/Components/Servicesection.jsx
@@ -1,10 +1,15 @@
+import dbConnect from '@/lib/dbConnect';
 import React from 'react';
 
-const Servicesection = () => {
+const Servicesection =async () => {
+    
+    const servicescollection = dbConnect('ServicesDb')
+     const data = await servicescollection.find({}).toArray();
+    
     return (
         <div>
            <div>
-            <h1 className='text-4xl font-bold text-center'>Our Service Area</h1>
+            <h1 className='text-4xl font-bold text-center'>Our Service Area </h1>
             <p className='text-gray-400 text-sm text-center'>the majority have suffered alteration in some form, by injected  humour, or randomised words <br /> which don't look even slightly believable. </p>
            </div>
 
