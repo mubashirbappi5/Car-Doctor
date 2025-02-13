0aef217da45022d3e360c0325c826f0765eab362
mubashirbappi <mubashirbappi@gmail.com>
2025-02-13 22:03:04 +0600

    implement contact section

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    
+                </div>
+                <div>
+                <p>We are open monday-friday</p>
+                <h2>7:00 am - 9:00 pm</h2>
+                </div>
+                
+            </div>
+            
+        </div>
+    );
+};
+
+export default Contact;
