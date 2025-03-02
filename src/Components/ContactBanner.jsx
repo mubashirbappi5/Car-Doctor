9a1dad5b58e17c0f0044c8553d0cf6daa256ad90
mubashirbappi <mubashirbappi@gmail.com>
2025-03-03 05:08:29 +0600

    contact banner added

---
+++ src/Components/ContactBanner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ContactBanner = () => {
+    return (
+        <div className='bg-black rounded-lg p-10 flex justify-between '>
+          <div>
+            <h1>We are open</h1>
+          </div>
+          <div>
+            <h1>Have a Question/</h1>
+          </div>
+          <div>
+            <h1>Need a Repair?</h1>
+          </div>
+
+            
+        </div>
+    );
+};
+
+export default ContactBanner;
